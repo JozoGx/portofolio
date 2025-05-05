5be67f37030e9eb260a6e00d6a397122d42bacf2
Robertdrl_ <jozo.gx125@gmail.com>
2025-05-05 08:23:00 +0700

    all fix

--- data.js
+++ data.js
@@ -2,20 +2,20 @@ const personalData = {
     profilePhoto: "image/profile.png",
     portfolioProjects: [
       {
-        title: "Project One",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper, diam et pharetra scelerisque, elit nisl maximus mi, vitae elementum nisi massa dignissim massa.",
+        title: "Project P5",
+        description: "Sefoteria adalah game petualangan bergaya pixel-art yang mengisahkan perjalanan seorang ksatria pemberani dalam meraih mimpinya. Game ini dikembangkan menggunakan Godot Engine untuk sistem permainannya dan Libresprite untuk pembuatan aset grafis.",
         image: "image/project1.png",
         featured: true
       },
       {
-        title: "Project Two",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper, diam et pharetra scelerisque, elit nisl maximus mi, vitae elementum nisi massa dignissim massa.",
+        title: "Project Sinvent",
+        description: "Sistem Inventaris adalah aplikasi web yang dirancang untuk membantu pengelolaan data barang secara efisien dan terstruktur. Dikembangkan menggunakan Laravel dengan dukungan Livewire dan Jetstream, aplikasi ini menawarkan antarmuka yang responsif, fitur real-time, serta manajemen user yang aman.",
         image: "image/project2.png",
         featured: false
       },
       {
         title: "Project Three",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper, diam et pharetra scelerisque, elit nisl maximus mi, vitae elementum nisi massa dignissim massa.",
+        description: "MikrOnline adalah aplikasi web berbasis Streamlit untuk konfigurasi MikroTik secara online. Dengan antarmuka yang sederhana dan mudah diakses dari berbagai perangkat, MikrOnline memudahkan pengguna mengelola jaringan tanpa perlu Winbox atau CLI, menjadikannya solusi praktis dan efisien bagi semua kalangan.",
         image: "image/project3.png",
         featured: false
       }
